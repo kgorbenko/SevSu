74da9adc3f3837effca0ef7d571ed762bb9b7f29
astro6703 <astro6703@gmail.com>
2019-10-02 22:29:33 +0300

    Add completed testing lab 1

--- Тестирование ПО/ЛР1/TestingLab1/TestingLab1/StringOperations.cs
+++ Тестирование ПО/ЛР1/TestingLab1/TestingLab1/StringOperations.cs
@@ -1,7 +1,13 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 
 public static class StringOperations
 {
-    public static string RemoveAt(string source, int position) => source.Remove(position, 1);
+    public static string RemoveAt(string source, int position)
+    {
+        if (source == null) throw new ArgumentNullException(source);
+        
+        return source.Remove(position, 1);
+    }
 }
