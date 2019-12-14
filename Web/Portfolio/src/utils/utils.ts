f93b70828b7cde7d3422aa08b3e1ac278b6c98d5
Kirill Gorbenko <astro6703@gmail.com>
2019-12-14 14:24:10 +0300

    Add utils module

---
+++ Web/Portfolio/src/utils/utils.ts
@@ -0,0 +1,15 @@
+export const createElementWithClass = (elementType: string, className?: string) => {
+    const element = document.createElement(elementType);
+    if (className) {
+        element.className = className;
+    }
+    return element;
+}
+
+export const createElementWithInnerHTML = (elementType: string, innerHTML?: string) => {
+    const element = document.createElement(elementType);
+    if (innerHTML) {
+        element.innerHTML = innerHTML;
+    }
+    return element;
+};
