d528ede3462d17220b731c2c96f6c0f54553f0f8
Kirill Gorbenko <astro6703@gmail.com>
2020-01-11 01:56:43 +0300

    Complete lab6

--- ТПО/Project/Testing/Program.cs
+++ ТПО/Project/Testing/Program.cs
@@ -1,7 +1,10 @@
-﻿using Testing.Util;
+﻿using System;
+using Testing.Util;
 using Testing.UnitTesting;
 using System.IO;
+using System.Linq;
 using Testing.IntegrationTesting;
+using Testing.Model;
 
 namespace Testing
 {
@@ -10,12 +13,13 @@ namespace Testing
         private enum Labs
         {
             Lab3,
-            Lab4
+            Lab4,
+            Lab6
         }
-        
+
         public static void Main(string[] args)
         {
-            const Labs option = Labs.Lab4;
+            const Labs option = Labs.Lab6;
 
             switch (option)
             {
@@ -33,7 +37,33 @@ namespace Testing
                     testEngine.Run();
                     break;
                 }
+                case Labs.Lab6:
+                {
+                    var matrix = CreateMatrix();
+                    var sumsOfPositiveElementsInColumns = MatrixOperations.GetSumsOfPositiveElementsInColumns(matrix);
+
+                    Console.WriteLine(string.Join(" " , sumsOfPositiveElementsInColumns.Select(x => x.ToString())));
+
+                    break;
+                }
             }
+        }
+
+        private static int[,] CreateMatrix()
+        {
+            var random = new Random();
+            var matrix = new int[1000, 1000];
+
+            for (var i = 0; i < 1000; i++)
+            {
+                for (var j = 0; j < 1000; j++)
+                {
+                    matrix[i, j] = Enumerable.Range(0, random.Next(1, 1000)).Select(x => random.Next(int.MinValue, 1000))
+                        .Last();
+                }
+            }
+
+            return matrix;
         }
     }
 }
