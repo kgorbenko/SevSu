719e561fd2a94923f69ecde208f8c77cff1f68c4
astro6703 <astro6703@gmail.com>
2020-01-18 12:27:00 +0300

    Add ability to filter data grid

--- .Net/Lab4/PrintedEditionMdi/PrintedEditionMdi/ViewModels/PrintedEditionControlViewModel.cs
+++ .Net/Lab4/PrintedEditionMdi/PrintedEditionMdi/ViewModels/PrintedEditionControlViewModel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Data;
 using System.Windows.Input;
@@ -14,12 +11,13 @@ namespace PrintedEditionMdi.ViewModels
 {
     public class PrintedEditionControlViewModel : INotifyPropertyChanged
     {
-        private ICollectionView printedEditions;
+        private readonly ICollectionView printedEditionsView;
+        private ObservableCollection<PrintedEdition> printedEditions;
         private PrintedEdition selectedItem;
+        private string filter;
         private ICommand removeCommand;
         private ICommand saveCommand;
         private ICommand openCommand;
-        private ICommand filterCommand;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -29,10 +27,22 @@ namespace PrintedEditionMdi.ViewModels
 
         public PrintedEditionControlViewModel()
         {
-            printedEditions = new CollectionView(Enumerable.Empty<PrintedEdition>());
+            printedEditions = new ObservableCollection<PrintedEdition>();
+            printedEditionsView = CollectionViewSource.GetDefaultView(printedEditions);
+            printedEditionsView.Filter = x =>
+            {
+                if (string.IsNullOrEmpty(filter)) return true;
+
+                var printedEdition = x as PrintedEdition;
+
+                return printedEdition.Name.Contains(filter)
+                    || printedEdition.Author.Contains(filter)
+                    || printedEdition.Price.ToString().Contains(filter)
+                    || printedEdition.CreatedAt.ToString("d").Contains(filter);
+            };
         }
 
-        public ICollectionView PrintedEditions
+        public ObservableCollection<PrintedEdition> PrintedEditions
         {
             get => printedEditions;
             set
@@ -52,21 +62,33 @@ namespace PrintedEditionMdi.ViewModels
             }
         }
 
+        public string Filter
+        {
+            get => filter;
+            set
+            {
+                if (filter == value)
+                {
+                    return;
+                }
+
+                filter = value;
+                printedEditionsView.Refresh();
+                OnPropertyChanged(nameof(Filter));
+            }
+        }
+
         public ICommand RemoveCommand =>
-            removeCommand ??= new RelayCommand(obj => PrintedEditions =
-                                                   new CollectionView(PrintedEditions.Cast<object?>()
-                                                                          .Where(item => !item.Equals(SelectedItem))));
+            removeCommand ??= new RelayCommand(obj => PrintedEditions.Remove(selectedItem));
 
         public ICommand SaveCommand =>
-            saveCommand ??= new RelayCommand(obj => PrintedEditionSerializeHelper.Serialize(printedEditions as IEnumerable<PrintedEdition>,
+            saveCommand ??= new RelayCommand(obj => PrintedEditionSerializeHelper.Serialize(printedEditions,
                                                                                             obj as string));
 
         public ICommand OpenCommand =>
-            openCommand ??= new RelayCommand(obj => PrintedEditions = new CollectionView(PrintedEditionSerializeHelper.Deserialize(obj as string)));
-
-        public ICommand FilterCommand =>
-            filterCommand ??= new RelayCommand(obj => PrintedEditions.Filter = obj as Predicate<object>);
-            openCommand ??= new RelayCommand(obj => PrintedEditions = new CollectionView(
-                                                 PrintedEditionSerializeHelper.Deserialize(obj as string)));
+            openCommand ??= new RelayCommand(obj =>
+                                                 PrintedEditions =
+                                                     new ObservableCollection<PrintedEdition
+                                                     >(PrintedEditionSerializeHelper.Deserialize(obj as string)));
     }
 }
--- .Net/Lab4/PrintedEditionMdi/PrintedEditionMdi/Views/PrintedEditionControl.xaml.cs
+++ .Net/Lab4/PrintedEditionMdi/PrintedEditionMdi/Views/PrintedEditionControl.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System.Windows;
-using System.Windows.Data;
 using Microsoft.Win32;
-using PrintedEditionMdi.Models;
 using PrintedEditionMdi.ViewModels;
 
 namespace PrintedEditionMdi.Views
@@ -45,22 +43,6 @@ namespace PrintedEditionMdi.Views
             {
                 viewModel.OpenCommand.Execute(dialog.FileName);
             }
-        }
-
-        private void FilterButtonClick(object sender, RoutedEventArgs e)
-        {
-            if (!(DataContext is PrintedEditionControlViewModel viewModel)) return;
-
-            var filter = Filter.Text;
-            CollectionViewSource.GetDefaultView(viewModel.PrintedEditions).Filter = x =>
-            {
-                var printedEdition = x as PrintedEdition;
-
-                return printedEdition.Name.Contains(filter)
-                    || printedEdition.Author.Contains(filter)
-                    || printedEdition.Price.ToString().Contains(filter)
-                    || printedEdition.CreatedAt.ToString("d").Contains(filter);
-            };
         }
     }
 }
