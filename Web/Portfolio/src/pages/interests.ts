ac6fe1f3279d9c93e550ef8eecfaeac1768b63ca
Kirill Gorbenko <astro6703@gmail.com>
2020-01-27 03:08:19 +0300

    Introduce modal window

--- Web/Portfolio/src/pages/interests.ts
+++ Web/Portfolio/src/pages/interests.ts
@@ -2,9 +2,11 @@ import * as $ from 'jquery';
 
 import { updateClockOnInterval } from '../clock/clock';
 import { visitPage } from '../storage/storage';
+import modal from '../modal/modal';
 
 $(() => {
     visitPage('interests');
     updateClockOnInterval(document.getElementById('date'), document.getElementById('time'), 1000);
     $('.contents').on('click', (event) => $(event.target).next().toggle());
+    $('#dialog').on('click', () => modal({ confirmAction: () => alert('lol') }));
 });
