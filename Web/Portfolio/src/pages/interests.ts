8005279c446ceabfdc2fe151fb18a644310c71ca
astro6703 <astro6703@gmail.com>
2020-03-19 13:08:18 +0300

    Migrate interests page with modal

--- Web/Portfolio/src/pages/interests.ts
+++ Web/Portfolio/src/pages/interests.ts
@@ -4,6 +4,9 @@ import { updateClockOnInterval } from '../clock/clock';
 import { visitPage } from '../storage/storage';
 import modal from '../modal/modal';
 
+import '../scss/interests.scss';
+import '../modal/modal.scss';
+
 $(() => {
     visitPage('interests');
     updateClockOnInterval(document.getElementById('date'), document.getElementById('time'), 1000);
