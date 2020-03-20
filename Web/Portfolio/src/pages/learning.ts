575cb41f66198aff3b65a7b6e947ffede5b2cd9b
astro6703 <astro6703@gmail.com>
2020-03-20 12:34:30 +0300

    Migrate learning page to scss

--- Web/Portfolio/src/pages/learning.ts
+++ Web/Portfolio/src/pages/learning.ts
@@ -3,6 +3,8 @@ import * as $ from 'jquery';
 import { updateClockOnInterval } from '../clock/clock';
 import { visitPage } from '../storage/storage';
 
+import '../scss/learning.scss';
+
 $(() => {
     visitPage('learning');
     updateClockOnInterval(document.getElementById('date'), document.getElementById('time'), 1000);
