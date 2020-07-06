1b3ecc03a33e5d7180ba176d3ef82556c210f13d
Kirill Gorbenko <astro6703@gmail.com>
2020-07-06 13:04:28 +0300

    Migrate Home page to php

--- Web/Portfolio/app/Views/Home/Index.php
+++ Web/Portfolio/app/Views/Home/Index.php
@@ -1,1 +1,10 @@
-<p>View content is here!<p>
+<div class="main-content">
+    <header><h1>Portfolio</h1></header>
+    <section>
+        <header><h2>About me</h2></header>
+        <p><i>Gorbenko Kirill Nikolaevich.</i></p>
+    </section>
+    <section>
+        <header><h2>NEXT CHAPTER</h2></header>
+    </section>
+<div>
