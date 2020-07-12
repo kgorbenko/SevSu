c51d85ab6195c00349e20da89af6be2d36f3984a
Kirill Gorbenko <astro6703@gmail.com>
2020-07-12 12:50:53 +0300

    Introduce dummy post request handler

--- Web/Portfolio/app/Controllers/ContactController.php
+++ Web/Portfolio/app/Controllers/ContactController.php
@@ -7,4 +7,8 @@ class ContactController
     public function index() {
         ViewRenderer::render("Views/Contact/Index.php", "Contact me");
     }
+
+    public function contactPost() {
+        ViewRenderer::render("Views/Contact/Contact.php", "Contact me", $_POST);
+    }
 }
--- Web/Portfolio/app/Views/Contact/Index.php
+++ Web/Portfolio/app/Views/Contact/Index.php
@@ -5,7 +5,7 @@
         <h1>Mail Me!</h1>
     </header>
     <p>All fields are required! Hover over fields to see the additional requirements.</p>
-    <form action="" method="POST">
+    <form action="Contact" method="POST">
         <ol>
             <li>
                 <label for="full-name-input">Full Name</label>
