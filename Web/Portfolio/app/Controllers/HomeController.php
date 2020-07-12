72ac90a6445ca654851dd1292192ea8ff7a61adc
Kirill Gorbenko <astro6703@gmail.com>
2020-07-12 12:49:04 +0300

    Move photos array declaration to a dummy repository

--- Web/Portfolio/app/Controllers/HomeController.php
+++ Web/Portfolio/app/Controllers/HomeController.php
@@ -2,23 +2,22 @@
 
 include "Views/ViewRenderer.php";
 include "ViewModels/Home/AboutViewModel.php";
+include "Model/Repositories/PhotosRepository.php";
 
 class HomeController
 {
+    private $photosRepository;
+
+    public function __construct() {
+        $this->photosRepository = new PhotosRepository();
+    }
+
     public function index() {
         ViewRenderer::render("Views/Home/Index.php", "Homepage");
     }
 
     public function about() {
-        $photos = array(
-            "Bridge" => "../client-side/images/bridge.jpg",
-            "Greens" => "../client-side/images/green.jpeg",
-            "Statue" => "../client-side/images/statue.jpg",
-            "Cock" => "../client-side/images/cock.jpg",
-            "City" => "../client-side/images/city.jpg",
-            "Fire" => "../client-side/images/fire.jpg"
-        );
-        $viewModel = new AboutViewModel($photos);
+        $viewModel = new AboutViewModel($this->photosRepository->getAllPhotos());
         ViewRenderer::render("Views/Home/About.php", "About me", $viewModel);
     }
 
---
+++ Web/Portfolio/app/Model/Repositories/PhotosRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class PhotosRepository
+{
+    function getAllPhotos() {
+        return array(
+            "Bridge" => "../client-side/images/bridge.jpg",
+            "Greens" => "../client-side/images/green.jpeg",
+            "Statue" => "../client-side/images/statue.jpg",
+            "Cock" => "../client-side/images/cock.jpg",
+            "City" => "../client-side/images/city.jpg",
+            "Fire" => "../client-side/images/fire.jpg"
+        );
+    }
+}
